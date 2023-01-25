0cacc29408bbfb4eb55c4cba7cd5c2ef2d7589b6
WEBCREEK\tcurichumbi <tcurichumbi@webcreek.com>
2023-01-25 13:20:51 -0500

    initial commit

---
+++ Finance/ViewModel/MainVM.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Xml.Serialization;
+using Finance.Model;
+
+namespace Finance.ViewModel
+{
+    public class MainVM : INotifyPropertyChanged
+    {
+        public Posts Blog
+        {
+            get;
+            set;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainVM()
+        {
+            ReadRss();
+        }
+
+        public void ReadRss()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Posts));
+
+            using (WebClient client = new WebClient())
+            {
+                string xml = Encoding.Default.GetString(client.DownloadData("https://feeds.simplecast.com/54nAGcIl"));
+                //string xml = Encoding.Default.GetString(client.DownloadData("https://www.wavetribe.com/sitemap_blogs_1.xml"));
+                using (Stream reader = new MemoryStream(Encoding.UTF8.GetBytes(xml)))
+                {
+                    Blog = (Posts)serializer.Deserialize(reader);
+                }
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
